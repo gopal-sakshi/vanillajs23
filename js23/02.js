b31e08e48f6ac46548d314a17e30beffee1aab19
gopal612 <gopal612@gmail.com>
2023-01-02 15:57:53 +0530

    div02_info got green color

--- js23/02.js
+++ js23/02.js
@@ -8,5 +8,15 @@ div02_title.innerHTML = "div02"
 document.getElementById('div02_id').appendChild(div02_title);
 
 var div02_info = document.createElement('div');
+div02_info.class = "div02_info_class";
+div02_info.style.color = `green`;
 div02_info.innerHTML = "some Info, description"
 document.getElementById('div02_id').appendChild(div02_info);
+
+
+/*
+        div02
+        - div02_title
+        - div02_info
+
+*/
