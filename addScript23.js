ae72b261a0af87bb579d2e29cf4f6ac1919e6df9
gopal-sakshi <gopal612@gmail.com>
2023-06-14 15:49:26 +0530

    doing stuff

---
+++ addScript23.js
@@ -0,0 +1,7 @@
+var div02 = document.createElement('div');
+div02.innerHTML = `This is some div element that got added by a dynamic script on button click;`
+div02.style.border = `solid red 1px`;
+div02.style.width = `50%`;
+div02.style.padding = `10px`;
+div02.style.margin = `30px`
+document.body.appendChild(div02);
